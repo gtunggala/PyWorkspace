05dfe1a4485ccbcc8f311193882704dd157df843
gtunggala <gtunggala@gmail.com>
2024-11-20 15:27:49 -0500

    removed comments

--- NumericDataTypes.py
+++ NumericDataTypes.py
@@ -1,8 +1,8 @@
-#print (2 + 2)
-#print (4 -2)
-#print (2 * 2)
-#print (4 / 2)
-#print ("Python has three numeric types: int, float, and complex")
+print (2 + 2)
+print (4 -2)
+print (2 * 2)
+print (4 / 2)
+print ("Python has three numeric types: int, float, and complex")
 
 myValue=1
 print(myValue)
