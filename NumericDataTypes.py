cae135a45cd30dc2d0136a9041d62a7f6946674e
gtunggala <gtunggala@gmail.com>
2024-11-20 15:09:24 -0500

    labs 1 to 3

---
+++ NumericDataTypes.py
@@ -0,0 +1,25 @@
+#print (2 + 2)
+#print (4 -2)
+#print (2 * 2)
+#print (4 / 2)
+#print ("Python has three numeric types: int, float, and complex")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type" + str(type(myValue)))
+
+myValue=1.0
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type" + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type" + str(type(myValue)))
+
+myValue=2j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type" + str(type(myValue)))
